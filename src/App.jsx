9bf86d66d3b7b275e2fac2562f5db7653908d17d
Eduardo Marinho <emartins.marinho@gmail.com>
2023-09-15 18:04:30 -0300

    Sidebar adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./global.css"
+import styles from './components/App.module.css'
+
 import { Header } from "./components/Header"
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Eduardo Marinho" content="Fala pessoal!" />
+        </main>
+      </div>
     </>
   )
 }
