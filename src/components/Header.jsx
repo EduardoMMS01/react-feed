1493b25c3e6b231769b0048a3d71e211a2ddef86
Eduardo Marinho <emartins.marinho@gmail.com>
2023-09-15 15:08:39 -0300

    header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css"
 
+import logo from '../assets/react.svg'
+
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <h1>React Feed</h1>
+            <img src={logo} alt="Logo do React" />
         </header>
     )
 }
